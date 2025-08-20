0f913b8c2b247ceeefc807b741abf5da42969e75
DanieleGA76 <daniele.a@df.estudante.senai.br>
2025-08-20 15:39:59 -0300

    Primeiro commit no novo repositório

--- marketplace/models.py
+++ marketplace/models.py
@@ -14,6 +14,7 @@ from django.db import models
 class Membro(models.Model):
     email = models.CharField(max_length=50, null=False, blank=False),
     senha = models.CharField(max_length=50, null=False, blank=False)
-
-class Genero(models.Model):
     nome = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return "Nome [nome={self.nome}]"
